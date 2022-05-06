02353bf44b405b2a9ec050deafc6a55ec8ae51d6
Vojtěch Humpl <72192205+VojtaHumpl@users.noreply.github.com>
2022-05-06 13:55:48 +0200

    Excluded Program from Coverage (#7)
    * decorator
    
    * Update dotnet.yml

--- ChatBotServer/ChatBotServer/Program.cs
+++ ChatBotServer/ChatBotServer/Program.cs
@@ -2,34 +2,39 @@
 using ChatBotServer.Commands;
 using ChatBotServer.Helpers;
 using ChatBotServer.TCPCommunication;
+using System.Diagnostics.CodeAnalysis;
 using System.Net.Sockets;
 
 
 
+namespace ChatBotServer {
+	[ExcludeFromCodeCoverage]
+	internal class Program {
+		static void Main(string[] args) {
+			var server = new TCPServer();
+			var msgHandler = new MessageHandler();
+			server.OnMessageReceived += Server_OnMessageReceived;
 
 
-
-var server = new TCPServer();
-var msgHandler = new MessageHandler();
-server.OnMessageReceived += Server_OnMessageReceived;
+			Task.Run(() => server.StartServer("127.0.0.1", 6969));
 
 
-Task.Run(() => server.StartServer("127.0.0.1", 6969));
+			while (true) {
+				Thread.Sleep(500);
+			}
 
 
-while (true) {
-	Thread.Sleep(500);
-}
-
-
-void Server_OnMessageReceived(object? sender, EventArgs e) {
-	var data = (MessageEventArgs)e;
-	var message = ProtocolParser.ParsePacket(data.Data);
-	var cmd = msgHandler.AnalyzeMessage(message);
-	var res = msgHandler.GetResponsePacketToMessage(cmd);
-	server.SendData(res);
-	Thread.Sleep(100);
-	server.RestartConnection();
+			void Server_OnMessageReceived(object? sender, EventArgs e) {
+				var data = (MessageEventArgs)e;
+				var message = ProtocolParser.ParsePacket(data.Data);
+				var cmd = msgHandler.AnalyzeMessage(message);
+				var res = msgHandler.GetResponsePacketToMessage(cmd);
+				server.SendData(res);
+				Thread.Sleep(100);
+				server.RestartConnection();
+			}
+		}
+	}
 }
 
 
